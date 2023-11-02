3906a64ea5d79693ae3af85b8f24cb71e74b8769
soonwoooo <kkshjs123@gmail.com>
2023-11-02 11:52:04 +0900

    중간저장

--- src/view/application/ProfileTitle.tsx
+++ src/view/application/ProfileTitle.tsx
@@ -1,8 +1,24 @@
 import styled from "styled-components";
 const ProfileTitle = () => {
-  return <ProfileTitleWrapper>123</ProfileTitleWrapper>;
+  return (
+    <ProfileTitleWrapper>
+      <BackGroundImage></BackGroundImage>
+    </ProfileTitleWrapper>
+  );
 };
 
 export default ProfileTitle;
 
-const ProfileTitleWrapper = styled.div``;
+const ProfileTitleWrapper = styled.div`
+  width: 850px;
+  height: 360px;
+  border-radius: 4px;
+  background-color: #fff;
+  border: 1px solid pink;
+  box-shadow: 0px 20px 60px 0px rgba(241, 244, 248, 0.5);
+`;
+const BackGroundImage = styled.div`
+  width: 100%;
+  height: 180px;
+  border: 1px solid blue;
+`;
